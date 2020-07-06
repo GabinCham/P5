afae0470e13176392a5c28c243abe11d03ad8a50
GabinCham <gabin.chameroy@hotmail.com>
2020-07-06 18:59:24 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var sujet = ['Serge', 'John', 'Kurt', 'Mick'];
+var verbe = ['fait de la guitar', 'joue de la batterie', 'fait du piano', 'fait du triangle'];
+var complement = ['à la plage.','sur scène.', 'sur la tour Eiffel.', 'nul part.'];
+
+alert('VERBE N3 == ', verbe[3]);
+
+const container = document.getElementById('container');
+
+container.innerHTML = verbe[3];
+
+class Quotes {
+    constructor() {
+        this.themeOne = {}
+    }
+    initQuotes() {
+
+    }
+}
