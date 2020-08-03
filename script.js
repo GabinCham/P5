ac50d72326f772becbdb86a72c7a395ede9ed643
GabinCham <gabin.chameroy@hotmail.com>
2020-08-03 11:27:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -52,11 +52,11 @@ function resetContainer() {
  */
 function generateQuotes() {
     var getTheme = document.getElementById("choice-theme").value;
-    let realValue = parseInt(getTheme);
-    if (realValue === 1) {
+   // let realValue = parseInt(getTheme);
+    if (getTheme === 'Thème 1') {
         const themeOneQuote = document.getElementById('themeone');
         themeOneQuote.innerHTML += quotes.generateQuotesFromThemeOne();
-    } else if (realValue === 2) {
+    } else if (getTheme === 'Thème 2') {
         const themeTwoQuote = document.getElementById('themetwo');
         themeTwoQuote.innerHTML += quotes.generateQuotesFromThemeTwo();
     } else {
