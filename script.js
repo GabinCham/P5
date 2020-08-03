78d11473772d1dbe029032ec5a3cb0d1dc618220
GabinCham <gabin.chameroy@hotmail.com>
2020-08-03 18:17:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -47,6 +47,7 @@ class Quotes {
  */
 function generateQuotes() {
     document.getElementById("themeone").innerHTML = "";
+    document.getElementById("themetwo").innerHTML = "";
     var getTheme = document.getElementById("choice-theme").value;
     if (getTheme === 'Harry Potter') {
         const themeOneQuote = document.getElementById('themeone');
